e2bf45bd47fe775922ca79939f73a950cf08ddf1
Simon Pitt <si_pitt@yahoo.com>
2021-07-31 16:27:29 +0100

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+mod random;
+
+use random::Random;
+
+struct VehicleRegistry {
+}
+
+impl VehicleRegistry {
+    // fn generate_vehicle_id(self, length: u8) -> String {}
+
+    // fn generate_vehicle_license(self, id: String) -> String {
+    // 	fn get_() {
+	    
+    // 	} 
+    // }
+}
+
+
+fn main() {
+    println!("{}", Random::rand_alpha(2));
+    println!("{}", Random::rand_numeric(2));
+}
