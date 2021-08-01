d7cd1e36d473a0d6abb903119bfffc9b22c8bdc5
Simon Pitt <si_pitt@yahoo.com>
2021-08-01 11:55:04 +0100

    Replace String with &str except in the hashmap.

--- src/main.rs
+++ src/main.rs
@@ -13,69 +13,73 @@ struct VehicleInfo {
 
 impl VehicleInfo {
     fn new(brand: String, electric: bool, catalogue_price: i32) -> Self {
-	Self {
-	    brand,
-	    electric,
-	    catalogue_price,
-	}
+        Self {
+            brand,
+            electric,
+            catalogue_price,
+        }
     }
 
     fn compute_tax(&self) -> f32 {
-	let mut tax_percentage = 0.05;
-	if self.electric {
-	    tax_percentage = 0.02
-	}
+        let mut tax_percentage = 0.05;
+        if self.electric {
+            tax_percentage = 0.02
+        }
 
-	tax_percentage * self.catalogue_price as f32
+        tax_percentage * self.catalogue_price as f32
     }
 
     fn print(&self) {
-	println!("Brand: {}", &self.brand);
-	println!("Payable tax: {}", &self.compute_tax());
+        println!("Brand: {}", &self.brand);
+        println!("Payable tax: {}", &self.compute_tax());
     }
 }
 
-struct Vehicle {
+struct Vehicle<'a> {
     id: String,
     license_plate: String,
-    info: VehicleInfo,
+    info: &'a VehicleInfo,
 }
 
-impl Vehicle {
-    fn new(id: String, license_plate: String, info: VehicleInfo) -> Self {
-	Self {
-	    id,
-	    license_plate,
-	    info,
-	}
+impl<'a> Vehicle<'a> {
+    fn new(id: String, license_plate: String, info: &'a VehicleInfo) -> Self {
+        Self {
+            id,
+            license_plate,
+            info,
+        }
     }
 
     fn print(&self) {
-	println!("Id: {}", self.id);
-	println!("License plate: {}", self.license_plate);
-	self.info.print();
+        println!("Id: {}", self.id);
+        println!("License plate: {}", self.license_plate);
+        self.info.print();
+	println!("");
     }
 }
 
 struct VehicleRegistry {
-    vehicle_info: HashMap<String, VehicleInfo>
+    vehicle_info: HashMap<String, VehicleInfo>,
 }
 
 impl VehicleRegistry {
     fn new() -> Self {
-	let mut map = HashMap::new();
-	VehicleRegistry::add_vehicle_info(&mut map, "Tesla Model 3", true, 60000);
-	VehicleRegistry::add_vehicle_info(&mut map, "Volkswagen ID3", true, 35000);
-	VehicleRegistry::add_vehicle_info(&mut map, "BMW 5", false, 45000);
-	VehicleRegistry::add_vehicle_info(&mut map, "Tesla Model Y", true, 75000);
-	Self {
-	    vehicle_info: map,
-	}
+        let mut map = HashMap::new();
+        VehicleRegistry::add_vehicle_info(&mut map, "Tesla Model 3", true, 60000);
+        VehicleRegistry::add_vehicle_info(&mut map, "Volkswagen ID3", true, 35000);
+        VehicleRegistry::add_vehicle_info(&mut map, "BMW 5", false, 45000);
+        VehicleRegistry::add_vehicle_info(&mut map, "Tesla Model Y", true, 75000);
+        Self { vehicle_info: map }
     }
 
-    fn add_vehicle_info(map: &mut HashMap<String, VehicleInfo>, brand: &str, electric: bool, catalogue_price: i32) {
-	let v = VehicleInfo::new(brand.to_string(), electric, catalogue_price);
-	map.insert(brand.to_string(), v);
+    fn add_vehicle_info(
+        map: &mut HashMap<String, VehicleInfo>,
+        brand: &str,
+        electric: bool,
+        catalogue_price: i32,
+    ) {
+        let v = VehicleInfo::new(brand.to_string(), electric, catalogue_price);
+        map.insert(brand.to_string(), v);
     }
 
     fn generate_vehicle_id(&self, length: u8) -> String {
@@ -92,26 +96,26 @@ impl VehicleRegistry {
     }
 
     fn create_vehicle(&self, brand: &str) -> Vehicle {
-	let id:String = self.generate_vehicle_id(12);
-	let license_plate: String = self.generate_vehicle_license(&id);
-	let info: VehicleInfo = self.vehicle_info.get(brand).unwrap().clone();
-	return Vehicle::new(id, license_plate, info)
-	}
+        let id: String = self.generate_vehicle_id(12);
+        let license_plate: String = self.generate_vehicle_license(&id);
+        let info = self.vehicle_info.get(brand).unwrap();
+        return Vehicle::new(id, license_plate, &info);
+    }
 }
 
 struct Application {}
 
 impl Application {
     fn register_vehicle(&self, brand: &str) {
-	// create a registry instance
-	let registry = VehicleRegistry::new();
-	let vehicle = registry.create_vehicle(brand);
-	vehicle.print();
+        // create a registry instance
+        let registry = VehicleRegistry::new();
+        let vehicle = registry.create_vehicle(brand);
+        vehicle.print();
     }
 }
 
 fn main() {
-    let app = Application{};
+    let app = Application {};
     app.register_vehicle("Volkswagen ID3");
     app.register_vehicle("Tesla Model 3");
     app.register_vehicle("BMW 5");
